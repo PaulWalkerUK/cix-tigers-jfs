9b6bd05690b9a5e8318daa8a1ecf68d5bf65c225
PaulWalkerUK <walker.paul@gmail.com>
2023-09-20 22:58:12 +0100

    Replace command-line parameters with a GUI

--- Update-CixTigersJfsVariables.ps1
+++ Update-CixTigersJfsVariables.ps1
@@ -3,53 +3,35 @@
 Update JoinFS configuration for 2-4-CIX Tigers
 
 .DESCRIPTION
-Sets the JoinFS variables file for all Tiger Moth models. The file "Plane-CixTigerMoth.txt" needs manually putting
-into relevant "Documents\JoinFS-xxx\variables" directory first. This script will then map all Tiger Moth models to 
-use it.
-
-.PARAMETER FSX
-Update the FSX version of JoinFS. Note if neither `FSX` or `MSFS` are specified, it will update both if they exist
-
-.PARAMETER MSFS
-Update the MSFS version of JoinFS. Note if neither `FSX` or `MSFS` are specified, it will update both if they exist
-
-.EXAMPLE
-Update-CixTigersJfsVariables.ps1
-
-Update the variables file for both FSX and MSFS (depending what exists)
-
-.EXAMPLE
-Update-CixTigersJfsVariables.ps1 -FSX
-
-Update the variables file for only FSX
-
-.EXAMPLE
-Update-CixTigersJfsVariables.ps1 -MSFS
-
-Update the variables file for only MSFS
-
-.EXAMPLE
-Update-CixTigersJfsVariables.ps1 -FSX -MSFS
-
-Update the variables file for both FSX and MSFS
+Sets the JoinFS variables file for all Tiger Moth models, controlled by a simple
+GUI.
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
-param (
-    [switch]$FSX,
-    [switch]$MSFS
-)
+param()
 
 Function UpdateFiles {
     [CmdletBinding()]
     param (
         [parameter(Mandatory=$true)][string]$modelsFile,
-        [parameter(Mandatory=$true)][string]$variablesFile
+        [parameter(Mandatory=$true)][string]$variablesFile,
+        [parameter(Mandatory=$true)][string]$tigerMothvariablesFile
     )
 
-    If(-not (Test-Path $modelsFile)) {
-        Write-Warning "[$modelsFile] not found - skipping"
-        return
+    If(-not (Test-Path $modelsFile -PathType Leaf)) {
+        return "[$modelsFile] not found - skipping"
     }
+
+    If(-not (Test-Path $variablesFile -PathType Leaf)) {
+        return "[$modelsFile] not found - skipping"
+    }
+
+    If(-not (Test-Path (Split-Path -Path $tigerMothvariablesFile) -PathType Container)) {
+        return "[$(Split-Path -Path $tigerMothvariablesFile)] not found - skipping"
+    }
+
+    $scriptPath = (Get-Variable MyInvocation -Scope Script).Value.MyCommand.Path
+    $scriptDir = Split-Path $scriptPath
+    Copy-Item -Path (Join-Path -Path $scriptDir -ChildPath "Plane-CixTigerMoth.txt") -Destination $tigerMothvariablesFile
 
     $models = Get-Content $modelsFile | Where-Object{$_ -match "^Tiger Moth*"} | ForEach-Object{$_.split("\|")[0]}
     Write-Verbose "Tiger Moth models found: $($models.length)"
@@ -75,29 +57,110 @@ Function UpdateFiles {
 
     $data | Set-Content $variablesFile
     Write-Verbose "Final line count   : $((get-content $variablesFile).length)"
-    Write-Host "Written: $variablesFile" -ForegroundColor Green
+    return "$variablesFile"
 }
 
-If (-not($FSX -or $MSFS)) {
-    If(Test-Path $("$env:LOCALAPPDATA\JoinFS-FSX\models - Microsoft Flight Simulator X.txt")) {
-        $FSX = $true
+$msfsModelsFile = Join-Path -Path ([environment]::getfolderpath("LocalApplicationData")) -ChildPath "JoinFS-FS2020" | Join-Path -ChildPath "models - Microsoft Flight Simulator 2020.txt"
+$msfsVariablesFile = Join-Path -Path ([environment]::getfolderpath("LocalApplicationData")) -ChildPath "JoinFS-FS2020" | Join-Path -ChildPath "variables.txt"
+$msfsTigerMothvariablesFile = Join-Path -Path ([environment]::getfolderpath("MyDocuments")) -ChildPath "JoinFS-FS2020" | Join-Path -ChildPath "Variables" | Join-Path -ChildPath "Plane-CixTigerMoth.txt"
+
+
+$fsxModelsFile = Join-Path -Path ([environment]::getfolderpath("LocalApplicationData")) -ChildPath "JoinFS-FSX" | Join-Path -ChildPath "models - Microsoft Flight Simulator X.txt"
+$fsxVariablesFile = Join-Path -Path ([environment]::getfolderpath("LocalApplicationData")) -ChildPath "JoinFS-FSX" | Join-Path -ChildPath "variables.txt"
+$fsxTigerMothvariablesFile = Join-Path -Path ([environment]::getfolderpath("MyDocuments")) -ChildPath "JoinFS-FSX" | Join-Path -ChildPath "Variables" | Join-Path -ChildPath "Plane-CixTigerMoth.txt"
+
+
+#################
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$mainForm = New-Object System.Windows.Forms.Form
+$mainForm.StartPosition = [Windows.Forms.FormStartPosition]::CenterScreen
+$mainForm.Width = 350
+$mainForm.Height = 200
+$mainForm.AutoSize = $true
+$mainForm.Text = "CIX Tigers JoinFS Configurator"
+
+#region description
+$description = New-Object System.Windows.Forms.Label
+$description.Text = @"
+This program updates settings in JoinFS so the CIX Tigers
+smoke can be sent and received.
+
+Select the appropriate version(s) of JoinFS to update and
+press the Update button when ready
+"@
+$description.Location = New-Object System.Drawing.Point(10, 10)
+$description.AutoSize=$true
+$mainForm.Controls.Add($description)
+#endregion description
+
+#region msfscheckbox
+$msfsCheckbox = New-Object System.Windows.Forms.CheckBox
+#$msfsCheckbox.Size = New-Object System.Drawing.Size(250,200)
+$msfsCheckbox.AutoSize = $true
+$msfsCheckbox.Location = New-Object System.Drawing.Point(10, 90)
+if(Test-Path $msfsModelsFile) {
+    $msfsCheckbox.Text = "Microsoft Flight Simulator (MSFS 2020)"
+    $msfsCheckbox.Enabled = $true
+} else {
+    $msfsCheckbox.Text = "Microsoft Flight Simulator (MSFS 2020)`nThis version of JoinFS not detected"
+    $msfsCheckbox.Enabled = $false
+}
+$mainForm.Controls.Add($msfsCheckbox)
+#endregion msfscheckbox
+
+#region fsxcheckbox
+$fsxCheckbox = New-Object System.Windows.Forms.CheckBox
+#$fsxCheckbox.Size = New-Object System.Drawing.Size(250,50)
+$fsxCheckbox.AutoSize = $true
+$fsxCheckbox.Location = New-Object System.Drawing.Point(10, 125)
+
+
+if(Test-Path $fsxModelsFile) {
+    $fsxCheckbox.Text = "Flight Simulator X (FSX)"
+    $fsxCheckbox.Enabled = $true
+} else {
+    $fsxCheckbox.Text = "Flight Simulator X (FSX)`nThis version of JoinFS not detected"
+    $fsxCheckbox.Enabled = $false
+}
+$mainForm.Controls.Add($fsxCheckbox)
+#endregion fsxcheckbox
+
+#region updatebutton
+$updateButton = New-Object System.Windows.Forms.Button
+$updateButton.Text = "Update"
+$updateButton.Location = New-Object System.Drawing.Point(10, 165)
+$updateButton.Add_Click({
+    $doneSomat = $false
+    $msfsResult = "N/A"
+    $fsxResult = "N/A"
+
+    if($msfsCheckbox.Checked) {
+        $msfsResult = UpdateFiles -modelsFile $msfsModelsFile -variablesFile $msfsVariablesFile -tigerMothvariablesFile $msfsTigerMothvariablesFile
+        $doneSomat = $true
     }
 
-    If(Test-Path $("$env:LOCALAPPDATA\JoinFS-FS2020\models - Microsoft Flight Simulator 2020.txt")) {
-        $MSFS = $true
+    if($fsxCheckbox.Checked) {
+        $fsxResult = UpdateFiles -modelsFile $fsxModelsFile -variablesFile $fsxVariablesFile -tigerMothvariablesFile $fsxTigerMothvariablesFile
+        $doneSomat = $true
     }
-}
 
-If($FSX) {
-    Write-Verbose "+-----+"
-    Write-Verbose "| FSX |"
-    Write-Verbose "+-----+"
-    UpdateFiles -modelsFile "$env:LOCALAPPDATA\JoinFS-FSX\models - Microsoft Flight Simulator X.txt" -variablesFile "$env:LOCALAPPDATA\JoinFS-FSX\variables.txt"
-}
+    if($doneSomat) {
+        [System.Windows.Forms.MessageBox]::Show($mainForm, "Complete. The following files have been updated:`n`n[MSFS]: $msfsResult`n`n[FSX]: $fsxResult", "Update", "OK", "Information")
+    } else {
+        [System.Windows.Forms.MessageBox]::Show($mainForm, "Nothing to do", "Update", "OK", "Warning")
+    }
+})
+$mainForm.Controls.Add($updateButton)
+#endregion updatebutton
 
-If($MSFS) {
-    Write-Verbose "+------+"
-    Write-Verbose "| MSFS |"
-    Write-Verbose "+------+"
-    UpdateFiles -modelsFile "$env:LOCALAPPDATA\JoinFS-FS2020\models - Microsoft Flight Simulator 2020.txt" -variablesFile "$env:LOCALAPPDATA\JoinFS-FS2020\variables.txt"
-}
+#region closebutton
+$closeButton = New-Object System.Windows.Forms.Button
+$closeButton.Text = "Close"
+$closeButton.Location = New-Object System.Drawing.Point(100, 165)
+$closeButton.Add_Click({$mainForm.Close()})
+$mainForm.Controls.Add($closeButton)
+#endregion closebutton
+
+$mainForm.ShowDialog()
